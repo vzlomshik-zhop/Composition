1b2f6320981932ebf77a1bab28d83e5b7478db68
vzlomshik-zhop <55409370+vzlomshik-zhop@users.noreply.github.com>
2020-01-06 15:22:18 +0200

    Update 1-pipe.js

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,18 @@
 'use strict';
 
-const pipe = (...fns) => x => null;
+const pipe = (...fns) => {
+  const n = fns.length;
+  let i = 0;
+  while (i < n && typeof(fns[i]) === "function")
+    i++;
+  if (i != n)
+    throw "Error";
+  return (x) => {
+    let a = x;
+    for (let i = 0; i < n; i++)
+      a = fns[i](a);
+    return a;
+  }
+}
 
 module.exports = { pipe };
