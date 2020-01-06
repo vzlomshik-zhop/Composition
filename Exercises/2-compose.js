f46de9215674f63d636d2558a9bc082c60c9a088
vzlomshik-zhop <55409370+vzlomshik-zhop@users.noreply.github.com>
2020-01-06 15:46:08 +0200

    Update 2-compose.js

--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,23 @@
 'use strict';
 
-const compose = (...fns) => x => null;
+const pipe = (...fns) => {
+  const n = fns.length;
+  let event_handler = (e) => undefined;
+  const fn = (x) => {
+    try {
+      let a = x;
+      for (let i = n - 1; i >= 0; i--)
+        a = fns[i](a);
+      return a;
+    } catch (e) {
+      return event_handler(e);
+    }
+  }
+  fn.on = (type, new_handler) => {
+    if (type === "error")
+      event_handler = new_handler;
+  }
+  return fn;
+}
 
 module.exports = { compose };
